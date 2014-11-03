f904dc6be3dfe1dbf9619331337d1ac0a92cc65c
anton@sk.ee <anton@sk.ee>
2014-11-03 10:21:17 +0200

    Add some scripts for multithread testing

--- smartcardpp-test/MultiThread/Run2OnMac.sh
+++ smartcardpp-test/MultiThread/Run2OnMac.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for (( i=0; i<5; i++))
+for (( i=0; i<2; i++))
 do
 	open -a /Applications/Utilities/Terminal.app/Contents/MacOS/Terminal "$HOME/src/smartcardpp_cmake/build/Debug/smartcardpp-test"
 	sleep 5
